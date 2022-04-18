2af528a2368e1f01204078df1e3095137d54a5b8
jyhwang <jyhwang@macrogen.com>
2022-04-18 18:45:40 +0900

    fix(Validation.java): 3자리수가 아닌 경우 Validation

--- src/main/java/baseball/Utils/Validation.java
+++ src/main/java/baseball/Utils/Validation.java
@@ -6,7 +6,7 @@ public class Validation {
     }
 
     public static boolean isValidation(String num) {
-        if (num.length() > 3) return false;
+        if (num.length() != 3) return false;
         if (!num.matches("[1-9]+")) return false;
         if (num.charAt(0) == num.charAt(1) ||
                 num.charAt(1) == num.charAt(2) ||
